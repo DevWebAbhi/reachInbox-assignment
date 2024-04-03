7af02429f144a6a3b14d86c099c84d669d73de35
DevWebAbhi <tiwariabhishek889912@gmail.com>
2024-04-03 12:27:31 +0530

    mailReader

--- mailReader.js
+++ mailReader.js
@@ -24,7 +24,6 @@ function readEmails(accessToken, refreshToken, profile) {
         console.log('User email ID:', emailId);
 
         gmail.users.messages.list({
-            userId: 'me',
             q: 'in:inbox',
         }, (err, res) => {
             if (err) {
@@ -35,7 +34,6 @@ function readEmails(accessToken, refreshToken, profile) {
             if (messages) {
                 messages.forEach((message) => {
                     gmail.users.messages.get({
-                        userId: 'me',
                         id: message.id,
                         format: 'full',
                     }, async (err, res) => {
@@ -48,16 +46,13 @@ function readEmails(accessToken, refreshToken, profile) {
 
                         let body = '';
 
-                        
                         if (messageData.payload.parts) {
                             const bodyPart = messageData.payload.parts.find(part => part.mimeType === 'text/html');
                             if (bodyPart) {
-                                
                                 const $ = cheerio.load(Buffer.from(bodyPart.body.data, 'base64').toString());
                                 body = $('body').text();
                             }
                         } else {
-                            
                             body = Buffer.from(messageData.payload.body.data, 'base64').toString();
                         }
 
@@ -65,22 +60,15 @@ function readEmails(accessToken, refreshToken, profile) {
                         if (arr.length == messages.length) {
                             arr.forEach(async (e, idx) => {
                                 try {
-                                    if (idx == 0) {
-                                        console.log(body);
-                                        await run(body, subject, emailId);
-                                        
-                                    }
-
+                                    console.log(body);
+                                    await run(body, subject, emailId);
                                 } catch (error) {
-                                    console.log(error)
+                                    console.log(error);
                                 }
-                            })
+                            });
                         }
-
                     });
-
                 });
-
             } else {
                 console.log('No messages found.');
             }
